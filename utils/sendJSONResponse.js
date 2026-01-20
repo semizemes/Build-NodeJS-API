d2bc0b32463dc496664f13a48fd895c82a6c8523
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-21 00:13:52 +0500

    refactor: add CORS headers to JSON response utility function

--- utils/sendJSONResponse.js
+++ utils/sendJSONResponse.js
@@ -1,5 +1,7 @@
 export default function sendJSONResponse(response, statusCode, payload) {
   response.setHeader("Content-Type", "application/json");
+  response.setHeader("Access-Control-Allow-Origin", "*")
+  response.setHeader("Access-Control-Allow-Method", "GET")
   response.statusCode = statusCode;
   response.end(JSON.stringify(payload));
 }
