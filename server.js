d6e2627d61c2f76ee5233962ec1e03b5761673f3
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-20 20:59:15 +0500

    refactor: enhance server request handling with async data fetching and improved filtering

--- server.js
+++ server.js
@@ -3,20 +3,44 @@ import { getDataFromDB } from "./database/db.js";
 import sendJSONResponse from "./utils/sendJSONResponse.js";
 import { getDataByPathParams } from "./utils/getDataByPathParams.js";
 
-const PORT = process.env.PORT || 8000
-const destinations = await getDataFromDB();
+const PORT = process.env.PORT || 8000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/api" && req.method === "GET") {
-    sendJSONResponse(res, 200, destinations);
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB();
+
+  const urlObj = new URL(req.url, `http://${req.headers.host}`);
+  const queryObj = Object.fromEntries(urlObj.searchParams);
+
+  if (urlObj.pathname === "/api" && req.method === "GET") {
+    let filteredData = destinations;
+
+    const hasItem = Object.keys(queryObj).length > 0;
+    console.log(queryObj);
+
+    if (hasItem) {
+      filteredData = filteredData.filter(
+        (i) => i.country.toLowerCase() == queryObj.country.toLowerCase()
+      );
+      console.log(filteredData);
+    }
+
+    sendJSONResponse(res, 200, filteredData);
   } else if (req.url.startsWith("/api/continent") && req.method === "GET") {
     const continent = req.url.split("/").pop().replace(/-/g, " ");
 
-    sendJSONResponse(res, 200, getDataByPathParams(destinations, "continent", continent));
-  } else if(req.url.startsWith("/api/country") && req.method === "GET"){
-    const country = req.url.split('/').pop().replace(/-/g, " ")
+    sendJSONResponse(
+      res,
+      200,
+      getDataByPathParams(destinations, "continent", continent),
+    );
+  } else if (req.url.startsWith("/api/country") && req.method === "GET") {
+    const country = req.url.split("/").pop().replace(/-/g, " ");
 
-    sendJSONResponse(res, 200, getDataByPathParams(destinations, "country", country))
+    sendJSONResponse(
+      res,
+      200,
+      getDataByPathParams(destinations, "country", country),
+    );
   } else {
     sendJSONResponse(res, 404, {
       error: "not found",
